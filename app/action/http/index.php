35f26636532884563d19fa983077c9dd786f7385
undefined <colin.wang@etocrm.com>
2019-12-02 15:04:22 +0800

    1

--- app/action/http/index.php
+++ app/action/http/index.php
@@ -6,6 +6,7 @@ class index
 {
     public function index($request,$response)
     {
+        echo 1;
         $arr = \service\artisan\config::get('app');
         print_r($arr);
 //        print_r(json_encode(compact('request','response')));
